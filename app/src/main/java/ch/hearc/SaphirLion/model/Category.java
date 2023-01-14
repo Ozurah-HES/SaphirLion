f31073eadb4e21f15e12e3a509b156f77594004c
Jonas Allemann <jonas.allemann@he-arc.ch>
2023-01-14 23:33:23 +0100

    chore: setup base DB integrations + base folder structure

---
+++ app/src/main/java/ch/hearc/SaphirLion/model/Category.java
@@ -0,0 +1,5 @@
+package ch.hearc.SaphirLion.model;
+
+public class Category {
+    
+}
---
+++ app/src/main/java/ch/hearc/SaphirLion/model/User.java
@@ -0,0 +1,28 @@
+package ch.hearc.SaphirLion.model;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    // Getters et setters
+}

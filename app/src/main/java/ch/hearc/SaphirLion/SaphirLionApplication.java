b64e1afd557e0e2a6796e481129f5ad55663fabf
sebastie.chappuis3 <sebastien.chappuis1@he-arc.ch>
2023-05-18 17:49:39 +0200

    feat: send HTTP request to localhost

--- app/src/main/java/ch/hearc/SaphirLion/SaphirLionApplication.java
+++ app/src/main/java/ch/hearc/SaphirLion/SaphirLionApplication.java
@@ -1,13 +1,28 @@
 package ch.hearc.SaphirLion;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ch.hearc.SaphirLion.model.Media;
+import ch.hearc.SaphirLion.service.impl.MediaService;
 import jakarta.jms.JMSException;
 import jakarta.jms.Message;
 import jakarta.jms.TextMessage;
@@ -15,9 +30,9 @@ import jakarta.jms.TextMessage;
 @SpringBootApplication
 public class SaphirLionApplication {
 
-	@Autowired
-	ObjectMapper mapper;
-	
+	@Value("${server.port}")
+	private String port;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SaphirLionApplication.class, args);
 	}
@@ -27,30 +42,51 @@ public class SaphirLionApplication {
 	 * @param jsonMessage
 	 * @throws JMSException
 	 */
-	//@JmsListener(destination = "${spring.activemq.json-queue}")
+	@JmsListener(destination = "${spring.activemq.media-json-queue}")
     public void readInprogressJsonMessage(final Message jsonMessage) throws JMSException {
         
-		String messageData = null;
-		
         System.out.println("Received message " + jsonMessage);
         
         if(!(jsonMessage instanceof TextMessage)) {
 			return;
 		}
 
-		Media media = null;
-		
 		TextMessage textMessage = (TextMessage)jsonMessage;
-		messageData = textMessage.getText();
+		String jsonString = textMessage.getText();
+
+		System.out.println("--------------------");
+		System.out.println("Received: " + jsonString);
+		System.out.println("--------------------");
+
+        // URL to send request to
+        String url = "http://localhost:" + port + "/api/media";
+
+        // Prepare headers (application/json)
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+		// Prepare request body (JSON)
+        HttpEntity<String> requestEntity = new HttpEntity<>(jsonString, headers);
+		HttpStatusCode statusCode = null;
+		String responseBody = null;
+
+		// Prepare RestTemplate
+        RestTemplate restTemplate = new RestTemplate();
 		
+		// Send request and get response as String (JSON) and status code
 		try {
-			media = mapper.readValue(messageData, Media.class);
-			
-			System.out.println(media);
-			
-		} catch (Exception e) {
-			System.out.println("Error while parsing JSON message");
+			ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, String.class);
+			statusCode = response.getStatusCode();
+        	responseBody = response.getBody();
+		} catch (HttpClientErrorException ex) {
+			statusCode = ex.getStatusCode();
+			responseBody = ex.getResponseBodyAsString();
 		}
-		System.out.println("messageData:"+messageData);
+
+        // Print status code and response body
+		System.out.println("--------------------");
+		System.out.println("<<< CODE: " + statusCode + " >>>");
+        System.out.println(responseBody);
+		System.out.println("--------------------");
     }
 }
